35e1e686ec545cf1c67a91ea713b1815effcfd2c
pcgiang <chaugiang0512@gmail.com>
2023-06-19 01:13:51 +0800

    first commit

---
+++ Desktop/Uni/Y3S2/summer/react-node-app/client/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [ data, setData ] = useState(null)
+
+  useEffect(() => {
+    fetch("/api")
+      .then(res => res.json())
+      .then((data) => setData(data.message))
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> { !data ? "Loading..." : data }</p>
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
